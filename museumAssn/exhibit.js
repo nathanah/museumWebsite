9121aa39a6f3c6938dce1a42b0ad520fb0f634ec
Elisabeth Henkens <ellie@henkens.com>
2019-04-08 16:25:34 -0700

    Fixed button

--- museumAssn/exhibit.js
+++ museumAssn/exhibit.js
@@ -1,11 +1,11 @@
 function more() {
   var hide = document.getElementById("section2");
   var more = document.getElementById("more");
-  if (hide.style.display === "none") {
-    hide.style.display = "flex";
-    more.style.display = "none";
-  } else {
+  if (hide.style.display === "flex") {
     hide.style.display = "none";
     more.style.display = "flex";
+  } else {
+    hide.style.display = "flex";
+    more.style.display = "none";
   }
 }
