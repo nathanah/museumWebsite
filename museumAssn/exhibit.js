42e79e8a704675e905197afb4ab42e46ca45a71a
Elisabeth Henkens <ellie@henkens.com>
2019-04-10 16:32:15 -0700

    Update Button
    Fixed how hidden content works. Now hides the image on mobile.

--- museumAssn/exhibit.js
+++ museumAssn/exhibit.js
@@ -1,11 +1,15 @@
-function more() {
-  var hide = document.getElementById("section2");
-  var more = document.getElementById("more");
-  if (hide.style.display === "flex") {
-    hide.style.display = "none";
-    more.style.display = "flex";
-  } else {
-    hide.style.display = "flex";
-    more.style.display = "none";
-  }
+function more(){
+  document.getElementById("more").style.display = "none";
+  document.getElementById("sketch").classList.remove("sketch");
+  document.getElementById("section2").classList.remove("hidden");
+  document.getElementById("sketch").classList.add("shown");
+  document.getElementById("section2").classList.add("shown");
+}
+
+function less(){
+  document.getElementById("more").style.display = "flex";
+  document.getElementById("sketch").classList.remove("shown");
+  document.getElementById("section2").classList.remove("shown");
+  document.getElementById("sketch").classList.add("sketch");
+  document.getElementById("section2").classList.add("hidden");
 }
