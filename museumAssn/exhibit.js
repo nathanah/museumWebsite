1a91e2e241d92d1ca0f3f5e1acd9b18d272f2a1d
Elisabeth Henkens <ellie@henkens.com>
2019-04-10 18:48:32 -0700

    Text Wrapping
    Text wrapping done.
    Flexbox only active in mobile mode for section 1 and section 2. Restructed section1/2 a bit.

--- museumAssn/exhibit.js
+++ museumAssn/exhibit.js
@@ -1,15 +1,13 @@
 function more(){
   document.getElementById("more").style.display = "none";
-  document.getElementById("sketch").classList.remove("sketch");
+  document.getElementById("section1image").classList.remove("sketch");
   document.getElementById("section2").classList.remove("hidden");
-  document.getElementById("sketch").classList.add("shown");
   document.getElementById("section2").classList.add("shown");
 }
 
 function less(){
   document.getElementById("more").style.display = "flex";
-  document.getElementById("sketch").classList.remove("shown");
   document.getElementById("section2").classList.remove("shown");
-  document.getElementById("sketch").classList.add("sketch");
+  document.getElementById("section1image").classList.add("sketch");
   document.getElementById("section2").classList.add("hidden");
 }
